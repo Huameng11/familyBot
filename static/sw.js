906a4c658c5958dd9aa2face174a17faa0ffdeaf
hua <15188636576@163.com>
2026-07-29 18:11:05 +0800

    基本功能全部完成不再更新

--- static/sw.js
+++ static/sw.js
@@ -1,12 +1,43 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-  self.skipWaiting();
+const CACHE_NAME = 'familybot-cache-v3';
+
+// 移除了容易报404的 './'，并将 Vue 换成了 prod 生产版
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/vue@3/dist/vue.global.prod.js' 
+];
+
+self.addEventListener('install', event => {
+  // 强制立即接管控制权
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
+self.addEventListener('activate', event => {
+  // 清理旧版本的缓存
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/api/')) return;
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
